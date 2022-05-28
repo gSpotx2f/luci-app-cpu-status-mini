0b12775fdbba2efb5cd977b420212b237403b3a8
gSpot <gspot.x2f@gmail.com>
2022-05-29 01:55:16 +0300

    Minor improvements

--- htdocs/luci-static/resources/view/status/include/18_cpu.js
+++ htdocs/luci-static/resources/view/status/include/18_cpu.js
@@ -3,22 +3,22 @@
 'require fs';
 
 return baseclass.extend({
-	title: _('CPU Load'),
+	title : _('CPU Load'),
 
-	load: function() {
+	load  : function() {
 		return L.resolveDefault(fs.read('/proc/stat'), null);
 	},
 
 	render: function(cpuData) {
 		if(!cpuData) return;
 
-		let cpuStatArray = [];
+		let cpuStatArray     = [];
 		let statItemsArray = cpuData.trim().split('\n').filter(s => s.startsWith('cpu'));
 
 		for(let str of statItemsArray) {
 			let arr = str.split(/\s+/).slice(0, 8);
-			arr[0] = (arr[0] === 'cpu') ? Infinity : arr[0].replace('cpu', '');
-			arr = arr.map(e => Number(e));
+			arr[0]  = (arr[0] === 'cpu') ? Infinity : arr[0].replace('cpu', '');
+			arr     = arr.map(e => Number(e));
 			cpuStatArray.push([
 				arr[0],
 				arr[1] + arr[2] + arr[3] + arr[5] + arr[6] + arr[7],
@@ -39,8 +39,8 @@ return baseclass.extend({
 			let loadAvg = 0;
 			if('cpuStatusStatArray' in window) {
 				let idle = c[2] - window.cpuStatusStatArray[i][2];
-				let sum = c[1] - window.cpuStatusStatArray[i][1];
-				loadAvg = Math.round(100 * sum / (sum + idle));
+				let sum  = c[1] - window.cpuStatusStatArray[i][1];
+				loadAvg  = Math.round(100 * sum / (sum + idle));
 			};
 
 			cpuTable.append(
@@ -51,7 +51,7 @@ return baseclass.extend({
 					E('td', { 'class': 'td' },
 						E('div', {
 								'class': 'cbi-progressbar',
-								'title': loadAvg + '%',
+								'title': ('cpuStatusStatArray' in window) ? loadAvg + '%' : _('Calculating') + '...',
 							},
 							E('div', { 'style': 'width:' + loadAvg + '%' })
 						)
